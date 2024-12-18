a26d3a9c02404db6481f39f84c1d05ff5b997eb2
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-12-18 08:53:34 -0300

    feat: criado classe modelo de dados da série

---
+++ src/main/java/com/github/sidaorodrigues/screenmatch/model/DadosSerie.java
@@ -0,0 +1,8 @@
+package com.github.sidaorodrigues.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao,
+                         @JsonAlias("Year") String ano) { }
