43c073cab35e851f43b47f3949f16c90c67aef8a
Sidney Rodrigues <sidney.jrodrigues.lima@gmail.com>
2024-12-26 08:38:43 -0300

    feat: criando model de dados episodio e dados de temporada

--- src/main/java/com/github/sidaorodrigues/screenmatch/ScreenmatchApplication.java
+++ src/main/java/com/github/sidaorodrigues/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package com.github.sidaorodrigues.screenmatch;
 
+import com.github.sidaorodrigues.screenmatch.model.DadosEpisodio;
 import com.github.sidaorodrigues.screenmatch.model.DadosSerie;
+import com.github.sidaorodrigues.screenmatch.model.DadosTemporada;
 import com.github.sidaorodrigues.screenmatch.service.ConsumoAPI;
 import com.github.sidaorodrigues.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -25,5 +30,23 @@ public class ScreenmatchApplication implements CommandLineRunner {
 		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 
 		System.out.println(dados);
+
+		//dados episodio
+		DadosSerie dadosSerie = conversor.obterDados(json, DadosSerie.class);
+		System.out.println(dados);
+
+		json = consumoAPI.obterDados("https://www.omdbapi.com/?t=the+office&season=3&episode=10&apikey=42f9ab4");
+		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
+		System.out.println(dadosEpisodio);
+
+		//dados temporada
+		List<DadosTemporada> dadosTemporadas = new ArrayList<>();
+		for (int x=1; x<=dados.totalTemporadas(); x++) {
+			json = consumoAPI.obterDados("https://www.omdbapi.com/?t=the+office&season="+ x +"&apikey=42f9ab4");
+			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
+			dadosTemporadas.add(dadosTemporada);
+		}
+
+		dadosTemporadas.forEach(System.out::println);
 	}
 }
